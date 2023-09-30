ab78e88174a9d26786d7bb30e54251c14aa7a6c4
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-30 20:03:51 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+
+
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    let words = tutorial.split(" ");
+    let capitalizedWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(" ");
+  });
+}
